8088f49672369e7d082bbb24943bbc3d3e3437ff
Sasha <cahqba@gmail.com>
2020-04-24 19:57:51 +0300

    example

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,12 @@
 slovar = {'book':'книга', 'table':'стол', 'dictionary':'словарь'}
-slovar['book'] = 'книжка'
-slovar['orange'] = 'апельсин'
-print(slovar.values())
+print(slovar.items())
+# print(slovar.values())
 # for key, item in slovar.items():
 #     print("key: "+key)
 #     print("item: "+item)
 # print(list(slovar.items())[0])
+
+import random
+b = [1,2,3,4,5,6]
+random.shuffle(b)
+print(b)
--- translateGame.py
+++ translateGame.py
@@ -1,28 +1,37 @@
+import random
+
 dictionary = {}
 print("")
 while True:
-    key = input("Введите слово на английском или stop чтобы прекратить: ")
+    key = input("Введите слово на английском или stop чтобы прекратить: ").lower().strip()
     if key == 'stop':
         break
-    value = input("Введите его перевод или stop чтобы прекратить: ")
+    value = input("Введите его перевод или stop чтобы прекратить: ").lower().strip()
     if value == 'stop':
         break
     dictionary[key] = value
 
+dictionary = list(dictionary.items())
+random.shuffle(dictionary)
+dictionary = dict(dictionary)
 score = 0
 errors = 0
 
 for key, value in dictionary.items():
-    word = input("Введите перевод слова "+key+": ")
+    word = input("Введите перевод слова "+key+": ").lower().strip()
     if word == value:
         score += 1
+    else:
+        errors += 1
+    if errors == 3:
+        exit("Вы сделали более 3ёх ошибок, игра окончена")
 print(score)
 
 """
-Подсчёт ошибок
+Подсчёт ошибок+
 +Дружелюбие
 Перемешать элементы словаря
-Проигрыш при условии 3 ошибок
+Проигрыш при условии 3 ошибок+
 Перевести все вводные строки к нижнему регистру
 Удалить пробелы в вводных строках
 """
